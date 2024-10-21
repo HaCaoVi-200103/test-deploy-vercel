b3093255ffe0d413b405f7ea018393d806466dc2
ICaoVy <icaovy2001@gmail.com>
2024-10-21 22:41:33 +0700

    updaate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/api", (req, res) => res.send("API on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
